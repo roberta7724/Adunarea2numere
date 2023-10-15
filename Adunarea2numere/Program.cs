75a1e9176c63c95c7e6564a86ca636ef3257ef84
roberta7724 <roberta.bara@emanuel.ro>
2023-10-15 15:35:51 +0300

    Initial project setup

---
+++ Adunarea2numere/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Adunarea2numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int s;
+
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            s = a + b;
+
+            Console.WriteLine(s);
+        }
+    }
+}
